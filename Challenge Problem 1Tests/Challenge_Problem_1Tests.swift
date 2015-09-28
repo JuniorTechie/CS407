0aa5d0b9d6779d6f1032826861fc730de69269a6
Solomon Kim <asdfsolomon@yahoo.com>
2015-09-28 13:49:20 -0500

    Initial Commit

---
+++ Challenge Problem 1Tests/Challenge_Problem_1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Challenge_Problem_1Tests.swift
+//  Challenge Problem 1Tests
+//
+//  Created by Solomon Kim on 9/28/15.
+//  Copyright © 2015 Solomon Kim. All rights reserved.
+//
+
+import XCTest
+@testable import Challenge_Problem_1
+
+class Challenge_Problem_1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
